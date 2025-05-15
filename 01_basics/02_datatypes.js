2e76db097ec8e1cab9832299a2c23abc5af3ea43
abirajmain <145671062+abirajmain@users.noreply.github.com>
2025-05-15 18:23:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";     // treat all JS codes as newer version
+
+//alert(3+3)      // we are using nodejs, not browser
+
+//console.log(5*5)   // code readability should be high
+//console.log("Ajmain")
+
+
+let name = "Abir Ajmain" // string
+let age = 20             // number
+let IsLoggedIn = true    // boolean
+
+let district = null
+console.table([name, age, IsLoggedIn, district])
+
+console.log(typeof name)
