3a1a58261d9ff659b31708b071300c611c01da4d
abirajmain <145671062+abirajmain@users.noreply.github.com>
2025-05-17 03:45:32 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+//***************************  Conversion  *****************************
+
 //let score = undefined
 
 //console.log(typeof score);
@@ -26,9 +28,26 @@
 // "Abir" => true
 
 
-let age = 23
-let stringNumber = String(age)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+//let age = 23
+//let stringNumber = String(age)
+//console.log(stringNumber);
+//console.log(typeof(stringNumber));
 
 
+//***************************  Operation  *****************************
+
+let num = 10
+let neg_value = -num
+console.log(neg_value)
+console.log(typeof(neg_value))
+
+console.log(10%5)
+
+let num1 = 7
+let num2 = 3
+let sum = num1 + num2
+console.log(sum);
+
+let point = 300
+point++
+console.log(point);
