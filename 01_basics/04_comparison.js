c36b66d9b9efd8fafd5dc053108186f69919f8d5
abirajmain <145671062+abirajmain@users.noreply.github.com>
2025-05-18 02:21:52 +0000

    comparison updated

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+//  comparison between same data type;
+
+//console.log(5 > 1)
+//console.log(6 < 4)
+//console.log(5 <= 5)
+//console.log(5 >= 5)
+//console.log(4 == 5)
+//console.log(6 != 5)
+
+//  comparison between different data types {string & number};
+
+//console.log("4" > 4)
+//console.log("05" < 8)
+
+//  comparison of null;
+
+// console.log(null > 5)  // here 'null' has treated as 0;
+// console.log(null == 0) // here 'null' has treated as 0;
+// console.log(null >= 0) // here 'null' has treated as 0;
+
+//  comparison of undefined;
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+// '==', '===' VS '<', '>'
+
+// they work differently;
